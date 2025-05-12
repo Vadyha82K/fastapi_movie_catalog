9dfc73121d747df93d6ca149e1d4db125a6a6c04
Vadim <120470243+Vadyha82K@users.noreply.github.com>
2025-05-12 21:38:41 +0300

    Added method validation by removing saving from safe methods

--- movie_catalog/api/api_v1/movies/dependencies.py
+++ movie_catalog/api/api_v1/movies/dependencies.py
@@ -1,13 +1,26 @@
 import logging
 
-from fastapi import HTTPException, BackgroundTasks
-from starlette import status
+from fastapi import (
+    HTTPException,
+    BackgroundTasks,
+    status,
+    Request,
+)
 
 from api.api_v1.movies.crud import storage
 from schemas.movie_description import MovieDescription
 
 
 log = logging.getLogger(__name__)
+
+UNSAFE_METHODS = frozenset(
+    {
+        "POST",
+        "PUT",
+        "PATCH",
+        "DELETE",
+    }
+)
 
 
 def prefetch_movie(movie_slug: str) -> MovieDescription:
@@ -24,7 +37,10 @@ def prefetch_movie(movie_slug: str) -> MovieDescription:
 
 def save_storage_state(
     background_tasks: BackgroundTasks,
+    request: Request,
 ):
+    log.info("Методом запроса является %r", request.method)
     yield
-    log.info("Добавил background tasks в save storage")
-    background_tasks.add_task(storage.save_state)
+    if request.method in UNSAFE_METHODS:
+        log.info("Добавил background tasks в save storage")
+        background_tasks.add_task(storage.save_state)
