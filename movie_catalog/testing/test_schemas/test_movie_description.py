1fd4538a22b8caaf7b4f6b440b324c068d1c40d1
Vadim <120470243+Vadyha82K@users.noreply.github.com>
2025-06-16 11:31:57 +0300

    Добавил проверку совместимости схем

---
+++ movie_catalog/testing/test_schemas/test_movie_description.py
@@ -0,0 +1,76 @@
+from unittest import TestCase
+
+from schemas.movie_description import (
+    MovieDescription,
+    MovieDescriptionCreate,
+    MovieDescriptionUpdate,
+)
+
+
+class MovieDescriptionCreateTestCase(TestCase):
+    def test_movie_description_can_be_created_from_create_schema(self) -> None:
+        movies_in = MovieDescriptionCreate(
+            slug="some-slug",
+            title="some-title",
+            description="some-description",
+            genre=["some-genre", "some-genre"],
+            release_year=2025,
+        )
+
+        movies = MovieDescription(**movies_in.model_dump())
+
+        self.assertEqual(
+            movies_in.slug,
+            movies.slug,
+        )
+
+        self.assertEqual(
+            movies_in.title,
+            movies.title,
+        )
+
+        self.assertEqual(
+            movies_in.description,
+            movies.description,
+        )
+
+        self.assertEqual(
+            movies_in.genre,
+            movies.genre,
+        )
+
+        self.assertEqual(
+            movies_in.release_year,
+            movies.release_year,
+        )
+
+
+class MovieDescriptionUpdateTestCase(TestCase):
+    def test_movie_description_can_be_created_from_update_schema(self) -> None:
+        movies_in = MovieDescriptionUpdate(
+            title="some-title",
+            description="some-description",
+            genre=["some-genre", "some-genre"],
+            release_year=2025,
+        )
+        movies = MovieDescription(slug="some-slug", **movies_in.model_dump())
+
+        self.assertEqual(
+            movies_in.title,
+            movies.title,
+        )
+
+        self.assertEqual(
+            movies_in.description,
+            movies.description,
+        )
+
+        self.assertEqual(
+            movies_in.genre,
+            movies.genre,
+        )
+
+        self.assertEqual(
+            movies_in.release_year,
+            movies.release_year,
+        )
