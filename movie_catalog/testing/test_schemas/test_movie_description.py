5ce7e0e6f0493160848a6264d9353b5cb7ccdf53
Vadim <120470243+Vadyha82K@users.noreply.github.com>
2025-06-18 12:55:40 +0300

    Добавил проверки (release_year, genre, title), при создании описания фильма

--- movie_catalog/testing/test_schemas/test_movie_description.py
+++ movie_catalog/testing/test_schemas/test_movie_description.py
@@ -44,9 +44,86 @@ class MovieDescriptionCreateTestCase(TestCase):
             movies.release_year,
         )
 
+    def test_movie_description_accepts_different_titles(self) -> None:
+        titles = [
+            "some title",
+            "the one",
+            "2015",
+            2015,
+            "The Shawshank Redemption",
+        ]
+
+        for title in titles:
+            with self.subTest(title=title, msg=f"test-title-{title}"):
+                movies_create = MovieDescriptionCreate(
+                    slug="some-slug",
+                    title=title,
+                    description="some-description",
+                    genre=["some-genre", "some-genre"],
+                    release_year=2025,
+                )
+                self.assertEqual(title, movies_create.model_dump(mode="json")["title"])
+
+    def test_movie_description_accepts_different_genre(self) -> None:
+        genre = [
+            [
+                "some genre",
+                "some genre",
+            ],
+            (
+                "some genre",
+                "some genre",
+            ),
+            [
+                "some genre",
+                34324,
+            ],
+            "Some genre",
+            [
+                "",
+            ],
+        ]
+
+        for item in genre:
+            with self.subTest(msg=f"test-genre-{item}"):
+                movies_create = MovieDescriptionCreate(
+                    slug="some-slug",
+                    title="some title",
+                    description="some-description",
+                    genre=item,
+                    release_year=2025,
+                )
+                self.assertEqual(item, movies_create.model_dump(mode="json")["genre"])
+
+    def test_movie_description_accepts_different_release_year(self) -> None:
+        release_year = [
+            2015,
+            0,
+            187333457985,
+            "2022",
+            1.998,
+        ]
+
+        for year in release_year:
+            with self.subTest(release_year=year, msg=f"test-genre-{year}"):
+                movies_create = MovieDescriptionCreate(
+                    slug="some-slug",
+                    title="some title",
+                    description="some-description",
+                    genre=[
+                        "some genre",
+                        "some genre",
+                    ],
+                    release_year=year,
+                )
+                self.assertEqual(
+                    year,
+                    movies_create.model_dump(mode="json")["release_year"],
+                )
+
 
 class MovieDescriptionUpdateTestCase(TestCase):
-    def test_movie_description_can_be_created_from_update_schema(self) -> None:
+    def test_movie_description_can_be_updated_from_update_schema(self) -> None:
         movies_in = MovieDescriptionUpdate(
             title="some-title",
             description="some-description",
