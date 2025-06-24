a3c6597c2b2459fb4b5f6ed13029b35ff20da040
Vadim <120470243+Vadyha82K@users.noreply.github.com>
2025-06-24 16:39:14 +0300

    Добавил параметризацию при вводе имени в корне

--- movie_catalog/testing/test_api/test_views.py
+++ movie_catalog/testing/test_api/test_views.py
@@ -1,3 +1,4 @@
+import pytest
 from fastapi import status
 from fastapi.testclient import TestClient
 
@@ -7,7 +8,23 @@ client = TestClient(app)
 
 
 def test_root_view() -> None:
-    name = "Vadim"
+    response = client.get("/")
+    assert response.status_code == status.HTTP_200_OK, response.text
+    expected_response = "Hello Guest!"
+    response_data = response.json()
+    assert expected_response == response_data["message"], response_data
+
+
+@pytest.mark.parametrize(
+    "name",
+    [
+        "Vadim",
+        "",
+        "Petr Ivanov",
+        "Ivan!$%^£",
+    ],
+)
+def test_root_view_custom_name(name: str) -> None:
     query = {"name": name}
     response = client.get("/", params=query)
     assert response.status_code == status.HTTP_200_OK, response.text
